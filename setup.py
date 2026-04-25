5ce23151e9345fdc42c6e414fe795938d4856871
KartikRaut09 <kartik.raut@somaiya.edu>
2026-04-25 20:31:27 +0530

    Add setup.py for pip install -e . support

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ecocloud-war-room",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "openenv-core",
+        "fastapi",
+        "uvicorn",
+        "pydantic>=2.0",
+        "matplotlib",
+        "numpy",
+    ],
+)
